0644c715b97b48e0a2a3ef56fe996fa644f365fd
Katirene <kat.traxler@gmail.com>
2016-01-30 16:48:04 -0600

    working easy mode

---
+++ app.js
@@ -0,0 +1,44 @@
+	$(document).ready(function() {
+		var empArray = [];
+
+		var totalAnnualSalaries = 0;
+		var monthlySalaries = 0;
+
+		$('#employeeForm').on('submit', function(event) {
+			event.preventDefault();
+		 	
+		 	//function to loop over values in array and store into our object values 
+		 	var values = {};
+		 	$.each($('#employeeForm').serializeArray(), function(i, field) {
+	 	 		values[field.name] = field.value;
+		 	});
+		 	totalAnnualSalaries += parseInt(values['empSalary']);
+	      	monthlySalaries = totalAnnualSalaries / 12;
+
+		 	$('#employeeForm').find('input[type=text]').val('');
+		 	$('#employeeForm').find('input[type=number]').val('');
+		 	appendDom(values);
+		 	appendTotal(monthlySalaries);
+	 	});
+
+
+		// function that appends input to dom.
+		 function appendDom(empInfo) {
+		 	$('#container').append('<div></div>');
+		 	var $el = $('#container').children().last();
+
+		 	$el.append('<p>' + empInfo.empFirstName + '</p>');
+		 	$el.append('<p>' + empInfo.empLastName + '</p>');
+		 	$el.append('<p>' + empInfo.empIDNumber + '</p>');
+		 	$el.append('<p>' + empInfo.empJobTitle + '</p>');
+		 	$el.append('<p>' + empInfo.empSalary + '</p>');
+		 }
+		 function appendTotal(newMonthly) {
+		 	$('#monthly').prop('disabled', false);
+		 	$('#monthly').val(newMonthly);
+		 	$('#monthly').prop('disabled', true);
+		 }
+
+	//console.log((empArray());
+
+	});
